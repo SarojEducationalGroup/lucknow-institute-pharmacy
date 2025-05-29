c68d7dec5fde61defd6466674c3bbf58183c7842
ShivamCo <shivam.singh.coo@gmail.com>
2025-05-29 12:51:35 +0530

    Chat Bot

--- lip/src/components/Layout.jsx
+++ lip/src/components/Layout.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import Header from './Header';
 import Footer from './Footer';
 import PopupForm from '../pages/PopUp';
+import Chatbot from './Chatbot';
 
 const Layout = ({children}) => {
     return (
         <div className='flex flex-col min-h-screen'>
             <PopupForm />
+            <Chatbot />
             {/* Header  */}
             <Header/>
     
